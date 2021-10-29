072c39ea5d480f4efceeba309a56a4617cc79eeb
Jeel Patel <jeelpatel231@gmail.com>
2021-10-30 02:23:59 +0530

    add Anilist API queries

--- app/src/main/java/ani/saikou/HomeFragment.kt
+++ app/src/main/java/ani/saikou/HomeFragment.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import ani.saikou.anilist.anilist
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
 
 class HomeFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -15,5 +18,12 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         viewInset(view.findViewById(R.id.fragment_home))
+        GlobalScope.async {
+            val lol = anilist.getQuery(anilist.queries.userData)
+            requireActivity().runOnUiThread{
+                view.findViewById<ProgressBar>(R.id.progressBar).visibility = View.GONE
+                view.findViewById<TextView>(R.id.textView).text = lol
+            }
+        }
     }
 }
--- app/src/main/java/ani/saikou/anilist/Anilist.kt
+++ app/src/main/java/ani/saikou/anilist/Anilist.kt
@@ -1,21 +1,23 @@
 package ani.saikou.anilist
 
 import android.content.Context
-import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import ani.saikou.MainActivity
-import ani.saikou.startMainActivity
-import java.io.File
 import androidx.browser.customtabs.CustomTabsIntent
-
-
+import ani.saikou.logger
+import ani.saikou.startMainActivity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+import org.jsoup.Jsoup
+import java.io.File
 
 
 var anilist : Anilist = Anilist()
 
 class Anilist {
+    val queries : AnilistQueries = AnilistQueries()
     var token : String? = null
 
     fun loginIntent(context: Context){
@@ -23,7 +25,7 @@ class Anilist {
 
         val builder = CustomTabsIntent.Builder()
         val customTabsIntent = builder.build()
-        customTabsIntent.launchUrl(context, Uri.parse("https://anilist.co/api/v2/oauth/authorize?client_id=$clientID&response_type=code"))
+        customTabsIntent.launchUrl(context, Uri.parse("https://anilist.co/api/v2/oauth/authorize?client_id=$clientID&response_type=token"))
     }
 
     fun getSavedToken(context: Context):Boolean{
@@ -33,12 +35,43 @@ class Anilist {
         }
         return false
     }
+
+    fun getQuery(query:String): String = runBlocking{
+        return@runBlocking withContext(Dispatchers.Default) {
+            Jsoup.connect("https://graphql.anilist.co/")
+                .header("Content-Type", "application/json")
+                .header("Accept", "application/json")
+                .header("Authorization", "Bearer $token")
+                .requestBody("""{"query":"$query"}""")
+                .ignoreContentType(true)
+                .ignoreHttpErrors(true)
+                .post().body().text()
+        }
+    }
+}
+
+class AnilistQueries{
+    val userData ="""{Viewer {name avatar{medium}bannerImage id statistics{anime{count}manga{count}}}}"""
+
+    fun continueWatching(userId:String,type:String): String {
+        return """ { MediaListCollection(userId: $userId, type: $type, status: CURRENT) { lists { entries { progress status media { id status chapters episodes nextAiringEpisode {episode} meanScore coverImage{large} title { userPreferred } } } } } } """
+    }
+    fun animeList(userId:String,type:String): String {
+        return """{ MediaListCollection(userId: $userId, type: $type) { lists { name entries { score(format:POINT_100) media { id status chapters episodes nextAiringEpisode {episode} meanScore coverImage{large} title { userPreferred } } } } } }"""
+    }
+
+    val recommendations = """ { Page(page: 1, perPage:25) { pageInfo { total currentPage hasNextPage } recommendations(sort: RATING_DESC, onList: true) { rating userRating mediaRecommendation { id title { userPreferred } format type status(version: 2) coverImage { large } } } } } """
+
+    fun coverImage(userId: String,type: String): String {
+        return """ query ($userId: Int){ MediaListCollection(userId: $userId, type: $type, sort:[SCORE_DESC,UPDATED_TIME_DESC],chunk:1,perChunk:1) { lists { entries{ media { coverImage{large} } } } } } """
+    }
 }
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val data: Uri? = intent?.data
+        logger(data.toString())
         anilist.token = Regex("""(?<=access_token=).+(?=&token_type)""").find(data.toString())!!.value
         val filename = "anilistToken"
         this.openFileOutput(filename, Context.MODE_PRIVATE).use {
